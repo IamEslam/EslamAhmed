dab233d7057b49c26dd687b4aae570a2d79a8b71
Eslam Ahmed <58029010+IamEslam@users.noreply.github.com>
2023-10-21 14:51:35 +0300

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -6,3 +6,34 @@
         $('body').delay(350).css({'overflow':'visible'});// إظهار جسم الصفحة
          
       });
+// ----------------------------------------------------------
+
+$(document).ready(function(){
+  $("#desc-1").click(function(){
+    $(".desc-1").toggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#desc-2").click(function(){
+    $(".desc-2").toggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#desc-3").click(function(){
+    $(".desc-3").toggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#desc-4").click(function(){
+    $(".desc-4").toggle("slow");
+  });
+});
+
+$(document).ready(function(){
+  $("#desc-5").click(function(){
+    $(".desc-5").toggle("slow");
+  });
+});
